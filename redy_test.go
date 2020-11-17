c2ccb93cf30f062b7190379d3ede6291654f8258
Anton Novojilov <andy@essentialkaos.com>
2020-11-17 13:36:33 +0300

    Improve GitHub Actions workflow

--- redy_test.go
+++ redy_test.go
@@ -583,12 +583,12 @@ func (rs *RedySuite) TestConfigParsers(c *C) {
 	c.Assert(cfg.Get("abc"), Equals, "")
 	c.Assert(cfg.Get(""), Equals, "")
 
-	fileConf, err := ReadConfig(".travis/test.conf")
+	fileConf, err := ReadConfig(".tests/test.conf")
 
 	c.Assert(err, NotNil)
 	c.Assert(fileConf, IsNil)
 
-	fileConf, err = ReadConfig(".travis/full.conf")
+	fileConf, err = ReadConfig(".tests/full.conf")
 
 	c.Assert(err, IsNil)
 	c.Assert(fileConf, NotNil)
